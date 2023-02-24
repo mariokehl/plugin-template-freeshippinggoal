fd931fdcbdbc2a9c92c0d747cabc5a3d386f895b
Mario Kehl <bureau@mariokehl.com>
2023-02-24 16:54:21 +0100

    Fix comparision of cost in shipping profile

--- src/Helpers/ShippingProfileHelper.php
+++ src/Helpers/ShippingProfileHelper.php
@@ -58,7 +58,7 @@ class ShippingProfileHelper
                     'subConstraintType',
                     '5' // Free Shipping or Flatrate
                 )->first();
-                if (!$constraint->cost) {
+                if (intval($constraint->cost) === 0) {
                     return (float)$constraint->startValue;
                 }
             }
